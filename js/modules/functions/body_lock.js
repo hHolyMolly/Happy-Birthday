f125370af15e6881141d3c7d703255ae8887a2c9
HolyMolly <104388974+hHolyMolly@users.noreply.github.com>
2024-03-23 18:36:35 +0200

    Add files via upload

--- js/modules/functions/body_lock.js
+++ js/modules/functions/body_lock.js
@@ -1,5 +1,27 @@
+const body = document.body;
+const wrapper = document.querySelector('.wrapper');
+const lockPadding = document.querySelectorAll('._lock-padding');
 
-const body=document.body;const wrapper=document.querySelector('.wrapper');const lockPadding=document.querySelectorAll('._lock-padding');export function bodyLock(){const lockPaddingValue=window.innerWidth-wrapper.offsetWidth+'px';if(lockPadding){lockPadding.forEach((elem)=>{elem.style.paddingRight=lockPaddingValue;});}
-body.style.paddingRight=lockPaddingValue;body.classList.add('_lock-scroll');}
-export function bodyUnLock(){if(lockPadding){lockPadding.forEach((elem)=>{elem.style.paddingRight='0px';});}
-body.style.paddingRight='0px';body.classList.remove('_lock-scroll');}
+export function bodyLock() {
+  const lockPaddingValue = window.innerWidth - wrapper.offsetWidth + 'px';
+
+  if (lockPadding) {
+    lockPadding.forEach((elem) => {
+      elem.style.paddingRight = lockPaddingValue;
+    });
+  }
+
+  body.style.paddingRight = lockPaddingValue;
+  body.classList.add('_lock-scroll');
+}
+
+export function bodyUnLock() {
+  if (lockPadding) {
+    lockPadding.forEach((elem) => {
+      elem.style.paddingRight = '0px';
+    });
+  }
+
+  body.style.paddingRight = '0px';
+  body.classList.remove('_lock-scroll');
+}
