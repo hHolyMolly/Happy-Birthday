e0e5136974cd11a612cdbe13b31716655e4710b4
HolyMolly <developer.code5417@gmail.com>
2024-03-23 18:20:19 +0200

    Updates

--- js/app.js
+++ js/app.js
@@ -1,2 +1,2 @@
 
-import'./modules/chunks/spoilers.js';import'./modules/chunks/modals.js';
+import'./modules/chunks/menu_burger.js';import'./modules/scripts.js';import'./modules/chunks/spoilers.js';import'./modules/chunks/modals.js';import'./modules/chunks/tabs.js';
